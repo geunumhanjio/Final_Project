7a8242196054982c932e666ab47633622ce2bfb8
gksqls1103 <gksqls1103@naver.com>
2026-02-03 18:59:46 +0900

    :card_file_box: [Mod] 디렉터리 수정

---
+++ FE/build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_VEDA_QT_1_FILE D:/work/QT_prac/VEDA_QT_1/FE/build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/VEDA_QT_1.exe)
+set(__QT_DEPLOY_TARGET_VEDA_QT_1_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_VEDA_QT_1_RUNTIME_DLLS C:/Qt/6.10.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.0/mingw_64/bin/Qt6Core.dll)
