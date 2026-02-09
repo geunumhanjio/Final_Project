21faf45bfc8961eaf20a1144a8a3b1b8530ec3c0
umdoyuun <tkdtlr1998@gmail.com>
2026-02-09 10:44:15 +0900

    :sparkles: [Feat] 카메라 브릿지

--- ros/WSL/src/camera_bridge/setup.py
+++ ros/WSL/src/camera_bridge/setup.py
@@ -1,3 +1,4 @@
+# camera_bridge/setup.py
 from setuptools import setup
 import os
 from glob import glob
@@ -6,20 +7,24 @@ package_name = 'camera_bridge'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.0.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        # launch 파일 추가
+        (os.path.join('share', package_name, 'launch'), 
+            glob('launch/*.launch.py')),
+        # config 파일 추가
+        (os.path.join('share', package_name, 'config'), 
+            glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='umdoyuun',
-    maintainer_email='tkdtlr1998@gmail.com',
-    description='Camera MJPEG bridge',
+    maintainer='your_name',
+    maintainer_email='your_email@example.com',
+    description='Camera bridge package',
     license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
