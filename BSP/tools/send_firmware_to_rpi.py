3a4c3d1fe546f357361728048ab9d90b9420892c
KIM, JIOH <joco9822@gmail.com>
2026-03-09 09:34:36 +0900

    :memo: [Docs] v5 버전에 맞게 rpi 가이드 수정

--- BSP/tools/send_firmware_to_rpi.py
+++ BSP/tools/send_firmware_to_rpi.py
@@ -3,7 +3,7 @@ import subprocess
 import sys
 
 RPI_USER = "iam"
-RPI_IP   = "192.168.0.28"          # 여기에 라즈베리파이 IP 입력
+RPI_IP   = "192.168.219.45"          # 여기에 라즈베리파이 IP 입력
 RPI_DEST = "~/"
 
 if not RPI_IP:
